0f3549b13765394be685c219ddae8d765d65eb26
FairyEver <1711467488@qq.com>
2020-06-25 22:46:09 +0800

    feat: ci 测试

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,33 @@ module.exports = {
   pluginOptions: {
     autoRouting: {
       chunkNamePrefix: 'page-'
+    },
+    electronBuilder: {
+      builderOptions: {
+        productName: 'Electron Vue Template',
+        artifactName: '${productName}-${version}.${ext}',
+        win: {
+          signAndEditExecutable: false,
+          target: ['nsis', 'portable'],
+        },
+        portable: {
+          artifactName: '${productName}-Portable-${version}.${ext}',
+        }
+      }
     }
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: 'src',
+          image: 'xlink:href',
+          'a-avatar': 'src'
+        }
+        return options
+      })
   }
 }
